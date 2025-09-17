abf2f3b9d6f2626f854f1ccf10e5631731fb38fc
Hebortinko <samo.hebort@gmail.com>
2025-09-17 10:26:59 +0200

    feat(Transaction): add Transaction Entity

--- src/main/java/org/example/bankingsystem/domain/Transaction.java
+++ src/main/java/org/example/bankingsystem/domain/Transaction.java
@@ -1,4 +1,47 @@
 package org.example.bankingsystem.domain;
 
+import jakarta.persistence.*;
+import java.math.BigDecimal;
+import java.time.OffsetDateTime;
+
+@Entity
 public class Transaction {
+
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(optional = false) private Account fromAccount;
+    @ManyToOne(optional = false) private Account toAccount;
+
+    @Column(nullable = false, precision = 19, scale = 4)
+    private BigDecimal amount;
+
+    @Column(nullable = false)
+    private OffsetDateTime createdAt = OffsetDateTime.now();
+
+    // getters & setter & constructor
+    public Transaction() {}
+    public Transaction(Account fromAccount, Account toAccount, BigDecimal amount) {
+        this.fromAccount = fromAccount;
+        this.toAccount = toAccount;
+        this.amount = amount;
+    }
+
+    // getters & setters
+    public Long getId() {
+        return id;
+    }
+    public Account getFromAccount() {
+        return fromAccount;
+    }
+    public Account getToAccount() {
+        return toAccount;
+    }
+    public BigDecimal getAmount() {
+        return amount;
+    }
+    public OffsetDateTime getCreatedAt() {
+        return createdAt;
+    }
+
 }
